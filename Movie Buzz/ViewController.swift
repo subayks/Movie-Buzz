7500cf151a803be277ac641926a7367cedf4ae75
Subay <Subay421@gmail.com>
2022-02-22 10:32:06 +0530

    Initial Commit

---
+++ Movie Buzz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Buzz
+//
+//  Created by Subendran on 22/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
